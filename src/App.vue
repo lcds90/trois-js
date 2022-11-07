f675969988fe443edaa8cd3b33e2b523f3199110
lcds90 <lcds90@gmail.com>
2022-11-06 23:38:15 -0300

    chore: initial project structure ✨💘

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <Renderer resize="window">
+    <Camera :position="{ z: 100 }" />
+    <Scene>     
+      <Box :size="10">
+        <BasicMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { Box, Camera, MeshPublicInterface, Renderer, RendererPublicInterface, Scene } from 'troisjs'
+
+const rendererC = ref()
+const meshC = ref()
+
+onMounted(() => {
+  const renderer = rendererC.value as RendererPublicInterface
+  const mesh = (meshC.value as MeshPublicInterface).mesh
+  renderer.onBeforeRender(() => {
+    mesh!.rotation.x += 0.01
+  })
+})
+</script>
+
+<style>
+body, html {
+  margin: 0;
+}
+canvas {
+  display: block;
+}
+</style>
